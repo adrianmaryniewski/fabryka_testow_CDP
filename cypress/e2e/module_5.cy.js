bc72a944338f4aca17e5a8eef2ace319e740fbf3
Adrian <a.maryniewski@gmail.com>
2022-12-20 19:08:45 +0100

    module_5

--- cypress/e2e/module_5.cy.js
+++ cypress/e2e/module_5.cy.js
@@ -1,6 +1,9 @@
-describe('My First Test', () => {
-    it('Visits the Kitchen Sink', () => {
+describe('Test suite', () => {
+    it('Should take a screenshot of a trainer', () => {
         cy.visit('https://fabrykatestow.pl')
+        cy.get('h2').contains('Dostępne szkolenia').scrollIntoView()
+        cy.get('span').contains('POKAŻ CZEGO SIĘ NAUCZĘ').eq(0).click()
+        cy.get('h1').contains('Kto nauczy CIę testów automatycznych?').scrollIntoView().screenshot()
         cy.log('Hello World')
     })
 })
